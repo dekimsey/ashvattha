8ea0d718cc425b1667f342a99640d6e17a5b43f4
Christian Maher <cmaher@acm.org>
2012-04-06 19:01:58 -0400

    example now easier to play with

--- src/example.cpp
+++ src/example.cpp
@@ -6,14 +6,20 @@
 #include <cmath>
 #include <cstdio>
 
+static const int width = 512;
+static const int height = 512;
+static const int nOctaves = 16;
+static const float  persistence = .5;
+static const float frequency = .007;
+
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
-    QImage img(512, 512, QImage::Format_RGB32);
+    QImage img(width, height, QImage::Format_RGB32);
     QLabel lbl;
     
-   for (int j = 0; j < 512; j++) {
-        for (int i = 0; i < 512; i++) {
-            int value = (int)SimplexNoise::octave(16, i, j , .5, .007, 0, 255);
+   for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            int value = (int)SimplexNoise::octave(nOctaves, i, j , persistence, frequency, 0, 255);
             QRgb color = qRgb(value, value, value);
             img.setPixel(i, j, color);
         }
